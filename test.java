dcf6929e0e656d80e30e36283228454272495348
Fardin Alam <143535484+Fardin4135@users.noreply.github.com>
2025-08-24 19:35:17 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,10 @@
+public class SubtractTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 30; // first number
+        int num2 = 10; // second number
+
+        int difference = num1 - num2; // subtraction
+
+        System.out.println("The difference is: " + difference);
+    }
+}
