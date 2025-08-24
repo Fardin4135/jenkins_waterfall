76295e2d2b35378735c665acc09337a8690e1c86
Fardin Alam <143535484+Fardin4135@users.noreply.github.com>
2025-08-24 19:35:53 +0530

    Create prod.java

---
+++ prod.java
@@ -0,0 +1,10 @@
+public class MultiplyTwoNumbers {
+    public static void main(String[] args) {
+        int num1 = 5;  // first number
+        int num2 = 6;  // second number
+
+        int product = num1 * num2; // multiplication
+
+        System.out.println("The product is: " + product);
+    }
+}
